6211499dcca2c280af6c2bf1c8f9f2875caaa2f6
Leila Demeter <89678876+artemisCeres@users.noreply.github.com>
2022-01-26 13:25:18 +0000

    Update func.c
    pointers fixed frfr

--- func.c
+++ func.c
@@ -1,18 +1,18 @@
 #include "func.h"
 #include <stdio.h>
 
-    void promptMin(int y){
+    void promptMin(int *y){
         printf("Enter minimum value: \n");
         scanf("%d", y);
     }
 
-    void promptMax(int y){
+    void promptMax(int *y){
         printf("Enter maximum value: \n");
         scanf("%d", y);
    
     }
 
-    void promptLength(int y){
+    void promptLength(int *y){
         printf("Enter desired quantity: \n"); 
         scanf("%d", y);
 
